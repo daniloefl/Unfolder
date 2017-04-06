a5045581d5ab6fdebfb9d1ec0054dae22db972e5
Danilo Ferreira de Lima <daniloefl@gmail.com>
2017-04-06 09:46:10 +0200

    Added setup.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='Unfolder',
+      version='0.1',
+      description='Software which unfolds a reconstructed distribution using the Fully Bayesian Unfolding method',
+      url='http://github.com/daniloefl/Unfolder',
+      author='Danilo Ferreira de Lima',
+      author_email='daniloefl@gmail.com',
+      license='GPL3.0',
+      packages=['Unfolder'],
+      zip_safe=False)
+
