af989125823c47512adc49567dcb089a70bc5345
Danilo Ferreira de Lima <daniloefl@gmail.com>
2017-04-29 10:15:58 +0200

    Bugfix.

--- unfoldFromROOT/unfoldBiasDagostini.py
+++ unfoldFromROOT/unfoldBiasDagostini.py
@@ -81,7 +81,7 @@ for i in ["", "me", "ps"]:
   bestAlphaBias[i] = -1
   bestAlphaBiasStd[i] = -1
   alpha[i], alphaChi2[i], bestAlphaBias[i], bestAlphaBiasStd[i] = scanRegParameter(DAgostiniForRegularizationTest(bkg[""], mig[""], eff[""]), bkg[i], mig[i], eff[i], truth[i], 1000, np.arange(1.0, 21.0, 1.0), "scanIter_DAgostini.png", "scanIter_chi2_DAgostini.png")
-  print "For configuration '%s': Found iter = %d with bias chi2 = %f, bias mean = %f, bias std = %f" % (alpha[i], alphaChi2[i], bestAlphaBias[i], bestAlphaBiasStd[i])
+  print "For configuration '%s': Found iter = %d with bias chi2 = %f, bias mean = %f, bias std = %f" % (i, alpha[i], alphaChi2[i], bestAlphaBias[i], bestAlphaBiasStd[i])
 
 pseudo_dagostini_mig = getDAgostini(bkg[i], mig[i], eff[i], pseudo_data, nIter = bestI[""])
 pseudo_dagostini_result = pseudo_dagostini_mig
