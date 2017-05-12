Fixed plotting y axis range calculation.

Unfolder/Histogram.py
@@ -532,8 +532,8 @@ def plotH1D(h, xlabel = "x", ylabel = "Events", title = "", fname = "plotH1D.png
   for n in h:
     k = h[n]
     plt.errorbar(k.x, k.val, k.err**0.5, k.x_err, fmt = sty[i], markersize=10, label = n)
-    if np.amax(k.val + k.err*0.5) > ymax: ymax = np.amax(k.val + k.err*0.5)
-    if np.amin(k.val - k.err*0.5) < ymin: ymin = np.amin(k.val - k.err*0.5)
+    if np.amax(k.val + k.err**0.5) > ymax: ymax = np.amax(k.val + k.err**0.5)
+    if np.amin(k.val - k.err**0.5) < ymin: ymin = np.amin(k.val - k.err**0.5)
     i += 1
   plt.ylabel(ylabel)
   plt.xlabel(xlabel)
@@ -556,8 +556,8 @@ def plotH1DLines(h, xlabel = "x", ylabel = "Events", title = "", fname = "plotH1
   for n in h:
     k = h[n]
     plt.errorbar(k.x, k.val, k.err**0.5, k.x_err, fmt = sty[i], markersize=10, label = n)
-    if np.amax(k.val + k.err*0.5) > ymax: ymax = np.amax(k.val + k.err*0.5)
-    if np.amin(k.val - k.err*0.5) < ymin: ymin = np.amin(k.val - k.err*0.5)
+    if np.amax(k.val + k.err**0.5) > ymax: ymax = np.amax(k.val + k.err**0.5)
+    if np.amin(k.val - k.err**0.5) < ymin: ymin = np.amin(k.val - k.err**0.5)
     i += 1
   plt.ylabel(ylabel)
   plt.xlabel(xlabel)